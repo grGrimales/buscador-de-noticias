febdd11abef651808111fde804f5b08b98b4829e
GrGrimales <grojasgrimales@gmail.com>
2021-09-25 21:05:08 -0500

    creada carperta docs

--- src/components/Noticia.js
+++ src/components/Noticia.js
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 
 export const Noticia = ({ noticia }) => {
-  const { urlToImage, url, title, description, source } = noticia;
+  const { urlToImage, url, title, description, source, publishedAt } = noticia;
 
   const imagen = urlToImage ? (
     <div className="card-image">
